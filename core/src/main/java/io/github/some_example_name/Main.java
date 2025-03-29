7815a8a990f504bf82d9dc1db30d5dd6c0080e40
foorcun <msnifoorcun@hotmail.com>
2025-03-30 00:16:05 +0300

    first milestone

---
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -0,0 +1,84 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import org.w3c.dom.Text;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main implements ApplicationListener {
+
+    Texture backgroundTexture;
+    Texture bucketTexture;
+    Texture dropTexture;
+    Sound dropSound;
+    Music music;
+
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
+    @Override
+    public void create() {
+        // Prepare your application here.
+        backgroundTexture = new Texture("background.png");
+        bucketTexture = new Texture("bucket.png");
+        dropTexture = new Texture("drop.png");
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8,5);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Resize your application here. The parameters represent the new window size.
+
+        viewport.update(width,height,true);
+    }
+
+    @Override
+    public void render() {
+        // Draw your application here.
+        input();
+        logic();
+        draw();
+    }
+
+    private  void input(){}
+    private void logic(){}
+    private void draw(){
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        spriteBatch.draw(bucketTexture, 0,0,1,1);
+
+
+
+
+        spriteBatch.end();
+    }
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy application's resources here.
+    }
+}
